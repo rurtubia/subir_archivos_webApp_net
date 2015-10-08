3868a6c5ef5b59b07979a4ee029149fec490d762
rurtubiac <randy.urtubia@inacapmail.cl>
2015-10-08 07:41:12 -0300

    Subir Archivos

---
+++ AppWeb1/index.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="AppWeb1.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:FileUpload ID="FileUpload1" runat="server" />
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ AppWeb1/index.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AppWeb1
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (FileUpload1.HasFile)
+            {
+                string ruta = Server.MapPath("~/imagenes/");
+
+                string ext = System.IO.Path.GetExtension(FileUpload1.FileName).ToLower();
+
+                string[] extensiones = { ".jpg", ".gif", ".png" };
+
+                bool valido = false;
+
+                for (int x = 0; x < extensiones.Length; x++)
+                {
+                    if (extensiones[x].Equals(ext))
+                    {
+                        valido = true;
+                        break;
+                    }
+                }
+                //FileUpload1.PostedFile.
+
+                if (valido)
+	            {
+		            FileUpload1.SaveAs(ruta + FileUpload1.FileName);
+                    Label1.Text = "Archivo subido exitosamente";
+	            }
+                else
+	            {
+                    Label1.Text ="Extensión nde archivo no soportado";
+	            }
+                
+            }
+            else
+            {
+                Label1.Text = "Debe seleccionar un archivo";
+            }
+        }
+    }
+}
